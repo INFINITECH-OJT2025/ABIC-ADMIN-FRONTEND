eba6b0a482b7be03ee617f3455a1e358dba7775c
l4nszxc <hernandezlanslorence@gmail.com>
2026-04-01 14:41:27 +0800

    fixed origin

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [100, 75],
   },
-  allowedDevOrigins: ["192.168.254.158", "192.168.254.150", "192.168.254.124" , "192.168.254.141", "192.168.254.108"],
+  allowedDevOrigins: ["abic-administrator.vercel.app"],
 };
 
 export default nextConfig;
